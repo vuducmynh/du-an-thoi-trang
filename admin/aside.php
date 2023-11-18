806c1c70357436d9b6d44821c563e94013dc6d9f
kiendev <nghiemminhkien2972003@gmail.com>
2023-11-18 15:48:10 +0700

    update admin

--- admin/aside.php
+++ admin/aside.php
@@ -15,27 +15,27 @@
                      </a>
                  </li>
                  <li class="sidebar-item">
-                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="index/act?=profile" aria-expanded="false">
+                     <a class="sidebar-link waves-effect waves-dark sidebar-link" href="profile.html" aria-expanded="false">
                          <i class="fa fa-user" aria-hidden="true"></i>
-                         <span class="hide-menu">Người dùng</span>
+                         <span class="hide-menu">Profile</span>
                      </a>
                  </li>
                  <li class="sidebar-item">
                      <a class="sidebar-link waves-effect waves-dark sidebar-link" href="basic-table.html" aria-expanded="false">
                          <i class="fa fa-table" aria-hidden="true"></i>
-                         <span class="hide-menu">Danh mục</span>
+                         <span class="hide-menu">Basic Table</span>
                      </a>
                  </li>
                  <li class="sidebar-item">
                      <a class="sidebar-link waves-effect waves-dark sidebar-link" href="fontawesome.html" aria-expanded="false">
                          <i class="fa fa-font" aria-hidden="true"></i>
-                         <span class="hide-menu">Sản phẩm</span>
+                         <span class="hide-menu">Icon</span>
                      </a>
                  </li>
                  <li class="sidebar-item">
                      <a class="sidebar-link waves-effect waves-dark sidebar-link" href="map-google.html" aria-expanded="false">
                          <i class="fa fa-globe" aria-hidden="true"></i>
-                         <span class="hide-menu">Đơn hàng</span>
+                         <span class="hide-menu">Google Map</span>
                      </a>
                  </li>
                  <li class="sidebar-item">
@@ -47,13 +47,13 @@
                  <li class="sidebar-item">
                      <a class="sidebar-link waves-effect waves-dark sidebar-link" href="404.html" aria-expanded="false">
                          <i class="fa fa-info-circle" aria-hidden="true"></i>
-                         <span class="hide-menu">Feedback</span>
+                         <span class="hide-menu">Error 404</span>
                      </a>
                  </li>
-                 <!-- <li class="text-center p-20 upgrade-btn">
+                 <li class="text-center p-20 upgrade-btn">
                      <a href="https://www.wrappixel.com/templates/ampleadmin/" class="btn d-grid btn-danger text-white" target="_blank">
                          Upgrade to Pro</a>
-                 </li> -->
+                 </li>
              </ul>
 
          </nav>
--- admin/index.php
+++ admin/index.php
@@ -1,48 +1,7 @@
-<?php
-require_once '../model/pdo.php';
-
-
-
-
-
-$act = $_GET['act'] ?? '';
-switch ($act) {
-    case 'home':
-        $VIEW = "dashboard/dashboard.php";
-        break;
-    case '':
-        $VIEW = "dashboard/dashboard.php";
-        break;
-    case '':
-        $VIEW = "dashboard/dashboard.php";
-        break;
-    case '':
-        $VIEW = "dashboard/dashboard.php";
-        break;
-    case '':
-        $VIEW = "dashboard/dashboard.php";
-        break;
-
-    default:
-        $VIEW = "dashboard/dashboard.php";
-        break;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+<?php 
 include 'header.php';
 include 'aside.php';
-//include $VIEW;
+include 'dashboard/dashboard.php';
 include 'footer.php';
+
+?>
