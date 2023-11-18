c87a9c1b866969f37765a791f60016e8cb66084d
kiendev <nghiemminhkien2972003@gmail.com>
2023-11-18 15:56:42 +0700

    update admin

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,48 @@
-<?php 
+<?php
+require_once '../model/pdo.php';
+
+
+
+
+
+$act = $_GET['act'] ?? '';
+switch ($act) {
+    case 'home':
+        $VIEW = "dashboard/dashboard.php";
+        break;
+    case '':
+        $VIEW = "dashboard/dashboard.php";
+        break;
+    case '':
+        $VIEW = "dashboard/dashboard.php";
+        break;
+    case '':
+        $VIEW = "dashboard/dashboard.php";
+        break;
+    case '':
+        $VIEW = "dashboard/dashboard.php";
+        break;
+
+    default:
+        $VIEW = "dashboard/dashboard.php";
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 include 'header.php';
 include 'aside.php';
-include 'dashboard/dashboard.php';
+//include $VIEW;
 include 'footer.php';
-
-?>
